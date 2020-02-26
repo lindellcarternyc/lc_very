a86bbc700afdc8d152ac6e19edcf1ad9e090d81f
Lindell Carter <lindellcarternyc@gmail.com>
2020-02-26 00:11:52 -0500

    push updates

---
+++ src/index.test.ts
@@ -0,0 +1,5 @@
+import { Verify } from './index'
+
+test('Verify', () => {
+  expect(Verify('hello')).toBe(true)
+})
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export const Verify = (data: any): boolean => {
+  return true
+}
